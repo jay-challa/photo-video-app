1f0fb8e1a6c1f77ffd7aef7227816bdbc53d5708
Jay <jay.challa@robosoftin.com>
2021-07-10 16:57:23 +0530

    API KEY

--- app/src/main/java/com/evaluation/photoandvideoapp/utils/Constants.kt
+++ app/src/main/java/com/evaluation/photoandvideoapp/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 	val EXPANDED = "EXPANDED"
 	val INTERNEDIATE = "INTERNEDIATE"
 	val COLLAPSED = "COLLAPSED"
-	var APIKEY = "563492ad6f917000010000013b7d021beddd4538821977af011b9e59"
+	var APIKEY = "********"
 	var data = "data.json"
 	var curated = "curated.json"
 	var video = "video.json"
